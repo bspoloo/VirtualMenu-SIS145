77db1e27a52191bb93833f160fcabbda26fab2ac
Alejandro Fernandez <luigiferca@gmail.com>
2023-06-17 13:37:48 -0400

    cambio de diseño del menu principal

--- app/src/main/java/com/example/virtualmenu/GaseosasActivity.kt
+++ app/src/main/java/com/example/virtualmenu/GaseosasActivity.kt
@@ -2,10 +2,52 @@ package com.example.virtualmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.virtualmenu.databinding.ActivityGaseosasBinding
+import com.example.virtualmenu.databinding.ActivitySopasBinding
+import com.example.virtualmenu.databinding.ActivityTesBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.ArrayList
 
 class GaseosasActivity : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
+    private lateinit var binding : ActivityGaseosasBinding
+
+    private lateinit var adapterprodct : AdapterMuestraProductos
+    private lateinit var producList : ArrayList<ItemProduct>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gaseosas)
+        binding = ActivityGaseosasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        llamarrecyclerview()
+    }
+    private fun llamarrecyclerview() {
+        producList = ArrayList()
+
+        adapterprodct = AdapterMuestraProductos(producList)
+
+        db.collection("Productos")
+            .whereEqualTo("Producto","Gaseosas")
+            .get()
+            .addOnSuccessListener { documets ->
+                for(document in documets){
+                    val wallItem = document.toObject(ItemProduct::class.java)
+                    wallItem.id = document.id
+                    wallItem.nom = document["Nombre"].toString()
+                    wallItem.tip = document["Tipo"].toString()
+                    wallItem.descp = document["Descripcion"].toString()
+                    wallItem.product = document["Producto"].toString()
+                    wallItem.pre = document["Precio"].toString().toInt()
+
+                    wallItem.imgProduct = document["Imagen"].toString()
+
+                    binding.recyclerssProductGaseosas.adapter = adapterprodct
+                    binding.recyclerssProductGaseosas.layoutManager = LinearLayoutManager(this)
+                    producList.add(wallItem)
+                }
+            }
+
     }
 }
--- app/src/main/java/com/example/virtualmenu/PostresActivity.kt
+++ app/src/main/java/com/example/virtualmenu/PostresActivity.kt
@@ -1,47 +1,58 @@
 package com.example.virtualmenu
 
-import CustomAdapter
 import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.virtualmenu.databinding.ActivityPostresBinding
+import com.example.virtualmenu.databinding.ActivitySopasBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.ArrayList
 
 class PostresActivity : AppCompatActivity() {
     val db = FirebaseFirestore.getInstance()
+    private lateinit var binding : ActivityPostresBinding
+
+    private lateinit var adapterprodct : AdapterMuestraProductos
+    private lateinit var producList : ArrayList<ItemProduct>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityPostresBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        llamarrecyclerview()
 
-        setContentView(R.layout.activity_postres)
 
-getData()
     }
+    private fun llamarrecyclerview() {
+        producList = ArrayList()
 
-    private fun getData() {
+        adapterprodct = AdapterMuestraProductos(producList)
 
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
-        recyclerview.layoutManager = LinearLayoutManager(this)
-
-        val data = ArrayList<ItemsViewModel>()
-
-        //Log.d(TAG,"Recuperando")
         db.collection("Productos")
+            .whereEqualTo("Producto","Postres")
             .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    data.add(ItemsViewModel("Nombre", document.data.get("Nombre").toString() ))
-                    Log.d(TAG, "${document.id} => ${document.data}")
+            .addOnSuccessListener { documets ->
+                for(document in documets){
+                    val wallItem = document.toObject(ItemProduct::class.java)
+                    wallItem.id = document.id
+                    wallItem.nom = document["Nombre"].toString()
+                    wallItem.tip = document["Tipo"].toString()
+                    wallItem.descp = document["Descripcion"].toString()
+                    wallItem.product = document["Producto"].toString()
+                    wallItem.pre = document["Precio"].toString().toInt()
 
+                    wallItem.imgProduct = document["Imagen"].toString()
+
+                    binding.recyclerssProductPostres.adapter = adapterprodct
+                    binding.recyclerssProductPostres.layoutManager = LinearLayoutManager(this)
+                    producList.add(wallItem)
                 }
+            }
 
-                val adapter = CustomAdapter(data)
-                recyclerview.adapter = adapter
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents.", exception)
-            }
     }
 }
